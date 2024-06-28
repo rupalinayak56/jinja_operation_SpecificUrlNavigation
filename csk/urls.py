bdc6fa94f12bfb8894afd743396dd95f20a60bd1
rupalinayak56 <rupalinayak7789@gmail.com>
2024-06-28 19:49:35 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from csk.views import *
+app_name='cskteam'
+urlpatterns=[
+    path('msd/',msd,name='msd'),
+]
---
+++ rcb/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from rcb.views import *
+app_name='rcbteam'
+urlpatterns=[
+    path('virat/',virat,name='virat'),
+]
